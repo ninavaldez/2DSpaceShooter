21a7d7a54309ebf846d1cc7d88f593f7b1d89107
ninavaldez <78568283+ninavaldez@users.noreply.github.com>
2021-03-27 21:02:03 -0700

    Initial commit

---
+++ Assets/Scripts/enemy_controller.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy_controller : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody2D rb;
+
+    private float timerbullet;
+    private float maxTimerbullet;
+    public GameObject bullet;
+
+
+    public float timerMin = 5f;
+    public float timerMax = 25f;
+    public bool canFireBullets = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = new Vector2(0, speed);
+
+        timerbullet = 0;
+        maxTimerbullet = Random.Range(timerMin, timerMax);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (canFireBullets)
+            StartCoroutine("FireBullet");
+
+        if (Camera.main.WorldToViewportPoint(transform.position).y < 0)
+            Destroy(this.gameObject);
+    }
+    void SpawnBullet()
+    {
+        Vector3 spawnPoint = transform.position;
+        spawnPoint.y -= (bullet.GetComponent<Renderer>().bounds.size.y / 2) + (GetComponent<Renderer>().bounds.size.y / 2);
+        GameObject.Instantiate(bullet, spawnPoint, transform.rotation);
+    }
+
+    IEnumerator FireBullet()
+    {
+        if (timerbullet >= maxTimerbullet)
+        {
+            //Spawn an enemy
+            SpawnBullet();
+            timerbullet = 0;
+            maxTimerbullet = Random.Range(timerMin, timerMax);
+        }
+
+        timerbullet += 0.1f;
+        yield return new WaitForSeconds(0.1f);
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# 2DSpaceShooter
+ 
